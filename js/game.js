d1f92e858f8a7af11adf58e19b29302650ddd5e7
adrianhuebner <adrianh92@gmail.com>
2019-07-29 14:39:12 -0700

    Adrian and James working form

--- js/game.js
+++ js/game.js
@@ -3,14 +3,15 @@
 var buttonEl = document.getElementById('gamestart');
 var modalEl = document.getElementById('modal');
 var selectionsEl = document.getElementById('selections');
+var formEl = document.getElementById('gameForm');
+var userName = '';
+var genderId = '';
 
-// buttonEl.onclick(console.log('click'));
-
-function startGame(event){
-  event.preventDefault();
-  console.log('clicked');
+formEl.addEventListener('submit', function(e){
+  e.preventDefault();
+  userName = e.target.userName.value;
+  genderId = e.target.pronoun.value;
+  console.log(userName, genderId);
   modalEl.style.display = 'none';
   selectionsEl.style.display = 'block';
-}
-
-buttonEl.addEventListener('click', startGame);
+});
