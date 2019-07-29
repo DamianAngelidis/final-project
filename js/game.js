6c061725b788aa4e878b398d16274dab8eb3f1c9
SteampunkMagic <damianangelidis@yahoo.com>
2019-07-29 15:41:07 -0700

    combined different game.js

--- js/game.js
+++ js/game.js
@@ -3,8 +3,26 @@
 var modalEl = document.getElementById('modal');
 var selectionsEl = document.getElementById('selections');
 var formEl = document.getElementById('gameForm');
+
+var questArray = [];
+var getPrompt = document.getElementById('prompt');
+
+var currentQuest = 1;
 var userName = '';
 var genderId = '';
+
+function Question( question, firstAnswer, secondAnswer) {
+    this.question = question;
+    this.firstAnswer = firstAnswer;
+    this.secondAnswer = secondAnswer;
+
+    questArray.push(this);
+}
+
+new Question('what is meaning of life?', 2, 3);
+new Question('what is food?', 4, 5);
+new Question('Am I alive?', 5, 6);
+
 
 // Switching modal start game
 formEl.addEventListener('submit', function(e){
@@ -15,6 +33,25 @@ formEl.addEventListener('submit', function(e){
   modalEl.style.display = 'none';
   selectionsEl.style.display = 'block';
 });
+
+function playGame() {
+    getPrompt.textContent = questArray[currentQuest - 1].question;
+    selectionsEl.addEventListener('click', handleChoice);
+}
+
+function handleChoice(event) {
+    console.log('I am working', event.target.id);
+    if(event.target.id === 'firstOption') {
+        currentQuest = questArray[currentQuest - 1].firstAnswer;
+    } else if(event.target.id === 'secondOption') {
+        currentQuest = questArray[currentQuest - 1].secondAnswer;
+    }
+    console.log(event.target.id);
+
+    playGame();
+}
+
+playGame();
 
 // // Constructor function for page layout
 // function Story(content,image,linkOne,linkTwo,contentOne,contentTwo){
