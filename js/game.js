4f57a8383a2084631298089f63c6640735c49b8b
James Dansie <jamesdansie@Jamess-MBP.localdomain>
2019-07-29 17:18:18 -0700

    working stored game data

--- js/game.js
+++ js/game.js
@@ -4,10 +4,8 @@ var modalEl = document.getElementById('modal');
 var selectionsEl = document.getElementById('selections');
 var formEl = document.getElementById('gameForm');
 var imgEl = document.getElementById('heroImage');
-
 var questArray = [];
 var getPrompt = document.getElementById('prompt');
-
 var currentQuest = 1;
 var userName = '';
 var genderId = '';
@@ -22,46 +20,21 @@ function Question( question, firstAnswer, secondAnswer, displayImg, endsGame) {
   questArray.push(this);
 }
 
-new Question('what is meaning of life?', 2, 3, 'https://via.placeholder.com/300x375', false);
-new Question('what is food?', 4, 5, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder3', 5, 6, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder4', 7, 8, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder5', 8, 9, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder6', 9, 10, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder7', 11, 13, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder8', 11, 12, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder9', 12, 13, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder10', 11, 13, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder11', 14, 15, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder12', 14, 15, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder13', 14, 15, 'https://via.placeholder.com/300x375', false);
-new Question('placeholder14', 5, 6, 'https://via.placeholder.com/300x375', true);
-new Question('placeholder15', 5, 6, 'https://via.placeholder.com/300x375', true);
-
-
-// Switching modal start game
-formEl.addEventListener('submit', function(e){
-  e.preventDefault();
-
-  //getting input data from form
-  userName = e.target.userName.value;
-  genderId = e.target.pronoun.value;
-
-  //input validation
-  if(userName.length === 0 || genderId.length === 0){
-    alert('Please input a name and choose a pronoun');
-  } else {
-    //switching the modal off
-    modalEl.style.display = 'none';
-    selectionsEl.style.display = 'block';
-  }
-});
+function storeData(key, data) {
+  var stringy = JSON.stringify(data);
+  localStorage.setItem(key, stringy);
+}
 
 function playGame() {
   //updating html based on the currentQuest value - 1
   getPrompt.textContent = questArray[currentQuest - 1].question;
   imgEl.src = questArray[currentQuest - 1].displayImg;
 
+  storeData('gameData', {
+    currentQuest: currentQuest,
+    userName: userName,
+    genderId: genderId
+  });
   //checking for end of game
   if(questArray[currentQuest - 1].endsGame === false) {
     selectionsEl.addEventListener('click', handleChoice);
@@ -81,5 +54,49 @@ function handleChoice(event) {
   }
   playGame();
 }
+
+
+new Question('what is meaning of life?', 2, 3, 'https://via.placeholder.com/300x375', false);
+new Question('what is food?', 4, 5, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder3', 5, 6, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder4', 7, 8, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder5', 8, 9, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder6', 9, 10, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder7', 11, 13, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder8', 11, 12, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder9', 12, 13, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder10', 11, 13, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder11', 14, 15, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder12', 14, 15, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder13', 14, 15, 'https://via.placeholder.com/300x375', false);
+new Question('placeholder14', 5, 6, 'https://via.placeholder.com/300x375', true);
+new Question('placeholder15', 5, 6, 'https://via.placeholder.com/300x375', true);
+
+
+// Switching modal to start game
+formEl.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  //getting input data from form
+  userName = e.target.userName.value;
+  genderId = e.target.pronoun.value;
+
+  //input validation
+  if(userName.length === 0 || genderId.length === 0){
+    alert('Please input a name and choose a pronoun');
+  } else {
+    
+    //Storing initial setup
+    storeData('gameData', {
+      currentQuest: currentQuest,
+      userName: userName,
+      genderId: genderId
+    });
+    
+    //switching the modal off
+    modalEl.style.display = 'none';
+    selectionsEl.style.display = 'block';
+  }
+});
 
 playGame();
